5a969c79e6d42a32e086c0f5e5bf78f74032aad3
Darren <darren.e.hunt@gmail.com>
2022-06-27 15:29:01 -0700

    check if stage is prod

--- handler.py
+++ handler.py
@@ -11,7 +11,7 @@ from botocore.exceptions import ClientError
 dynamodb = boto3.resource('dynamodb')
 
 projects_table = os.environ['PROJECTS_TABLE']
-stage = os.environ['STAGE']
+
 
 #=========================================#
 #=======     Helper Functions     ========#
@@ -42,6 +42,12 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 def removeProjectFromCalendarEvents(list_of_event_ids):
+    # The stage is used in some URLs. The production URL for the calendar
+    # is '...org/calendar...', so check first if the stage is 'prod'.
+    if os.environ['STAGE'] == 'prod':
+        stage = 'calendar'
+    else:
+        stage = os.environ['STAGE']
     calendarURL = f"https://calendar.photonranch.org/{stage}/remove-project-from-events"
     requestBody = json.dumps({
         "events": list_of_event_ids
